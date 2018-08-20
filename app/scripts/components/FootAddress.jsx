4bff4d75a12fbc1711b9a607ad93214b075d5a2c
kgentner <gentnerkarl@gmail.com>
2018-08-20 12:31:34 -0700

    slight footer updates

--- app/scripts/components/FootAddress.jsx
+++ app/scripts/components/FootAddress.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const FootAddress = () => (
   <div className="app__footaddress">
-    <div>AWSOM, PO BOX 2593, KIRKLAND, WA 98023
-      <span className="app__tab" />(425) 951-9758
-      <span className="app__tab" />HELLO@AWSOM.INFO
+    <div className="app__container">
+      AWSOM, PO BOX 2593, KIRKLAND, WA 98023
+      <span className="app__tab" />
+      (425) 951-9758
+      <span className="app__tab" />
+      HELLO@AWSOM.INFO
     </div>
   </div>
 );
