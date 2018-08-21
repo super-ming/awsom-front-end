86c830a1bec4dacb90459a60943450102dedc937
kgentner <gentnerkarl@gmail.com>
2018-08-21 16:09:49 -0700

    initial match page

--- app/scripts/routes/Match.jsx
+++ app/scripts/routes/Match.jsx
@@ -1,10 +1,35 @@
 import React, { Fragment } from 'react';
+import Header from '../components/Header';
 
 export default class Match extends React.Component {
   render() {
     return (
-      <Fragment >
-        <p> Match </p>
+      <Fragment>
+        <Header />
+        <div className="app__container">
+          <div className="app__matchfound">
+            <h1>We have a great match for you!</h1>
+            <p>
+              Please Confirm, Cancel, or Try Again to look for a different date.
+            </p>
+            <div className="app__matchdetails">
+              <div>Teacher: Mr. Shorofsky</div>
+              <div>Date: Friday, September 7, 2018</div>
+              <div>Time: 5:30pm</div>
+            </div>
+          </div>
+          <div className="app__matchbuttons">
+            <button type="submit" className="btn btn-primary">
+              CONFIRM
+            </button>
+            <button type="submit" className="btn btn-primary">
+              CANCEL
+            </button>
+            <button type="submit" className="btn btn-primary">
+              TRY AGAIN
+            </button>
+          </div>
+        </div>
       </Fragment>
     );
   }
