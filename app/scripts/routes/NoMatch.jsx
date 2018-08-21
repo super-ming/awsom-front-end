a6bccc8f674d9a9114e4d7eb244f74686e655360
kgentner <gentnerkarl@gmail.com>
2018-08-21 16:17:22 -0700

    initial NO MATCH page

--- app/scripts/routes/NoMatch.jsx
+++ app/scripts/routes/NoMatch.jsx
@@ -1,10 +1,21 @@
 import React, { Fragment } from 'react';
+import Header from '../components/Header';
 
 export default class NoMatch extends React.Component {
   render() {
     return (
-      <Fragment >
-        <p> NoMatch </p>
+      <Fragment>
+        <Header />
+        <div className="app__container">
+          <div className="app__matchfound">
+            <h1>We still haven't found... what you're looking for.</h1>
+            <p>Please contact us directly!</p>
+            <div className="app__matchdetails">
+              <div>Email: HELLO@AWSOM.INFO</div>
+              <div>Phone: (425) 951-9758</div>
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
