7c29baa8a694aad0fc1949bf20fd528c9484c9b2
kgentner <gentnerkarl@gmail.com>
2018-08-21 15:47:29 -0700

    initial confirmation page

--- app/scripts/routes/Confirmation.jsx
+++ app/scripts/routes/Confirmation.jsx
@@ -1,16 +1,27 @@
 import React, { Fragment } from 'react';
+import Header from '../components/Header';
 
 export default class Confirmation extends React.Component {
-  state = { matchTeacher: 'Mr. Shorofsky' };
+  // state = { matchTeacher: 'Mr. Shorofsky' };
 
   render() {
     return (
-      <Fragment >
-        <h1>Success!</h1>
-        <img src="http://placehold.it/150" alt="success" />
-        <p>A confirmation for the following appointment will be sent to your email address.</p>
-        <div className="match-details">
-          <div>Teacher: {this.state.matchTeacher}</div>
+      <Fragment>
+        <Header />
+        <div className="app__container">
+          <div className="app__confirmation">
+            <h1>Success!</h1>
+            <p>
+              A confirmation for the following appointment will be sent to your
+              email address.
+            </p>
+            <div className="match-details">
+              {/* <div>Teacher: {this.state.matchTeacher}</div> */}
+              <div>Teacher: Mr. Shorofsky</div>
+              <div>Date: Friday, September 7, 2018</div>
+              <div>Time: 5:30pm</div>
+            </div>
+          </div>
         </div>
       </Fragment>
     );
